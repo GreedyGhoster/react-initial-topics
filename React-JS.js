3a4d9f9945a2729c67f15c9c2f4c2fedd786f752
IuanPR <103894461+IuanPR@users.noreply.github.com>
2022-12-01 19:46:56 +0300

    Function Components > Class Components

--- React-JS.js
+++ React-JS.js
@@ -1,26 +1,30 @@
 const root = ReactDOM.createRoot(document.querySelector("#main-component"));
-function YourTime() {
-  return <h1>Время на момент открытия: {new Date().toLocaleTimeString()}</h1>;
+class YourTime extends React.Component {
+  render() {
+    return <h1>Время на момент открытия: {new Date().toLocaleTimeString()}</h1>;
+  }
 }
-function Greeting(props) {
-  return (
-    <div>
-      <h1>
-        Привет {props.FirstName} {props.LastName}
-      </h1>
-      <h1>Сегодняшняя дата: {new Date().toLocaleDateString()}</h1>
-      <YourTime />
-    </div>
-  );
+class Greeting extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Привет {this.props.FirstName} {this.props.LastName}
+        </h1>
+        <h1>Сегодняшняя дата: {new Date().toLocaleDateString()}</h1>
+        <YourTime />
+      </div>
+    );
+  }
 }
-
-function HelloUser() {
-  return (
-    <Greeting
-      FirstName={prompt("Введите ваше имя:")}
-      LastName={prompt("Введите вашу фамилию:")}
-    />
-  );
+class HelloUser extends React.Component {
+  render() {
+    return (
+      <Greeting
+        FirstName={prompt("Введите ваше имя:")}
+        LastName={prompt("Введите вашу фамилию:")}
+      />
+    );
+  }
 }
-
-root.render(HelloUser());
+root.render(<HelloUser />);
